6f865a496c5b557ac87d0133e52961142eb2ebe6
ayay230 <398529803@qq.com>
2021-06-18 11:37:29 +0800

    手机验证码登录后，在token中加入权限标识符

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysLoginWithPhoneController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysLoginWithPhoneController.java
@@ -3,13 +3,13 @@ package com.ruoyi.web.controller.system;
 import com.alibaba.fastjson.JSONObject;
 import com.ruoyi.common.config.AliSendSmsConfig;
 import com.ruoyi.common.core.domain.entity.SysUser;
+import com.ruoyi.common.core.domain.model.CommonResult;
 import com.ruoyi.common.core.domain.model.LoginUser;
 import com.ruoyi.common.core.redis.RedisCache;
 import com.ruoyi.common.utils.AliSendSmsUtils;
 import com.ruoyi.common.utils.DateUtils;
 import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.framework.web.service.TokenService;
-import com.ruoyi.common.core.domain.model.CommonResult;
 import com.ruoyi.system.service.ISysUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -18,7 +18,9 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -49,7 +51,7 @@ public class SysLoginWithPhoneController {
         //随机生成六位数验证码
         int smsCode = (int) (Math.random() * 1000000);
         //redis缓存
-        redisCache.setCacheObject("smsCode:" + phone, smsCode,60 * 5, TimeUnit.SECONDS);
+        redisCache.setCacheObject("smsCode:" + phone, smsCode, 60 * 5, TimeUnit.SECONDS);
         //实体封装
         AliSendSmsConfig aliSendSmsConfig = new AliSendSmsConfig();
         aliSendSmsConfig.setPhoneNumbers(phone);
@@ -92,6 +94,7 @@ public class SysLoginWithPhoneController {
             //实体封装
             LoginUser loginUser = new LoginUser();
             loginUser.setUser(sysUser1);
+            loginUser.setPermissions(getPermissionsSet());
             //获取token
             return CommonResult.ok().setData(tokenService.createToken(loginUser));
         } else {
@@ -110,8 +113,35 @@ public class SysLoginWithPhoneController {
             //实体封装
             LoginUser loginUser = new LoginUser();
             loginUser.setUser(sysUser1);
+            loginUser.setPermissions(getPermissionsSet());
             //获取token
             return CommonResult.ok().setData(tokenService.createToken(loginUser));
         }
+    }
+
+    //获取权限列表
+    public Set<String> getPermissionsSet() {
+        Set<String> permissions = new HashSet<>();
+        permissions.add("block:manager:list");
+        permissions.add("block:manager:add");
+        permissions.add("block:manager:edit");
+        permissions.add("block:manager:remove");
+        permissions.add("bill:manager:list");
+        permissions.add("bill:manager:add");
+        permissions.add("bill:manager:edit");
+        permissions.add("bill:manager:remove");
+        permissions.add("lease:manager:list");
+        permissions.add("lease:manager:add");
+        permissions.add("lease:manager:edit");
+        permissions.add("lease:manager:remove");
+        permissions.add("device:manager:list");
+        permissions.add("device:manager:add");
+        permissions.add("device:manager:edit");
+        permissions.add("device:manager:remove");
+        permissions.add("device:record:list");
+        permissions.add("device:record:add");
+        permissions.add("device:record:edit");
+        permissions.add("device:record:remove");
+        return permissions;
     }
 }
