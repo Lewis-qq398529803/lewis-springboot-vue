02201097bcaaf69cfbad8c83d2a0f004be3fc7c8
ayay230 <398529803@qq.com>
2021-06-22 16:50:46 +0800

    修改wxpayutil的inputstream2string方法

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/wxpay/sdk/WXPayUtil.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/wxpay/sdk/WXPayUtil.java
@@ -72,7 +72,7 @@ public class WXPayUtil {
         org.w3c.dom.Document document = WXPayXmlUtil.newDocument();
         org.w3c.dom.Element root = document.createElement("xml");
         document.appendChild(root);
-        for (String key: data.keySet()) {
+        for (String key : data.keySet()) {
             String value = data.get(key);
             if (value == null) {
                 value = "";
@@ -93,8 +93,7 @@ public class WXPayUtil {
         String output = writer.getBuffer().toString(); //.replaceAll("\n|\r", "");
         try {
             writer.close();
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
         }
         return output;
     }
@@ -104,7 +103,7 @@ public class WXPayUtil {
      * 生成带有 sign 的 XML 格式字符串
      *
      * @param data Map类型数据
-     * @param key API密钥
+     * @param key  API密钥
      * @return 含有sign字段的XML
      */
     public static String generateSignedXml(final Map<String, String> data, String key) throws Exception {
@@ -114,8 +113,8 @@ public class WXPayUtil {
     /**
      * 生成带有 sign 的 XML 格式字符串
      *
-     * @param data Map类型数据
-     * @param key API密钥
+     * @param data     Map类型数据
+     * @param key      API密钥
      * @param signType 签名类型
      * @return 含有sign字段的XML
      */
@@ -130,13 +129,13 @@ public class WXPayUtil {
      * 判断签名是否正确
      *
      * @param xmlStr XML格式数据
-     * @param key API密钥
+     * @param key    API密钥
      * @return 签名是否正确
      * @throws Exception
      */
     public static boolean isSignatureValid(String xmlStr, String key) throws Exception {
         Map<String, String> data = xmlToMap(xmlStr);
-        if (!data.containsKey(WXPayConstants.FIELD_SIGN) ) {
+        if (!data.containsKey(WXPayConstants.FIELD_SIGN)) {
             return false;
         }
         String sign = data.get(WXPayConstants.FIELD_SIGN);
@@ -147,7 +146,7 @@ public class WXPayUtil {
      * 判断签名是否正确，必须包含sign字段，否则返回false。使用MD5签名。
      *
      * @param data Map类型数据
-     * @param key API密钥
+     * @param key  API密钥
      * @return 签名是否正确
      * @throws Exception
      */
@@ -158,14 +157,14 @@ public class WXPayUtil {
     /**
      * 判断签名是否正确，必须包含sign字段，否则返回false。
      *
-     * @param data Map类型数据
-     * @param key API密钥
+     * @param data     Map类型数据
+     * @param key      API密钥
      * @param signType 签名方式
      * @return 签名是否正确
      * @throws Exception
      */
     public static boolean isSignatureValid(Map<String, String> data, String key, SignType signType) throws Exception {
-        if (!data.containsKey(WXPayConstants.FIELD_SIGN) ) {
+        if (!data.containsKey(WXPayConstants.FIELD_SIGN)) {
             return false;
         }
         String sign = data.get(WXPayConstants.FIELD_SIGN);
@@ -176,7 +175,7 @@ public class WXPayUtil {
      * 生成签名
      *
      * @param data 待签名数据
-     * @param key API密钥
+     * @param key  API密钥
      * @return 签名
      */
     public static String generateSignature(final Map<String, String> data, String key) throws Exception {
@@ -186,8 +185,8 @@ public class WXPayUtil {
     /**
      * 生成签名. 注意，若含有sign_type字段，必须和signType参数保持一致。
      *
-     * @param data 待签名数据
-     * @param key API密钥
+     * @param data     待签名数据
+     * @param key      API密钥
      * @param signType 签名方式
      * @return 签名
      */
@@ -206,11 +205,9 @@ public class WXPayUtil {
         sb.append("key=").append(key);
         if (SignType.MD5.equals(signType)) {
             return MD5(sb.toString()).toUpperCase();
-        }
-        else if (SignType.HMACSHA256.equals(signType)) {
+        } else if (SignType.HMACSHA256.equals(signType)) {
             return HMACSHA256(sb.toString(), key);
-        }
-        else {
+        } else {
             throw new Exception(String.format("Invalid sign_type: %s", signType));
         }
     }
@@ -248,8 +245,9 @@ public class WXPayUtil {
 
     /**
      * 生成 HMACSHA256
+     *
      * @param data 待处理数据
-     * @param key 密钥
+     * @param key  密钥
      * @return 加密结果
      * @throws Exception
      */
@@ -267,6 +265,7 @@ public class WXPayUtil {
 
     /**
      * 日志
+     *
      * @return
      */
     public static Logger getLogger() {
@@ -275,14 +274,16 @@ public class WXPayUtil {
 
     /**
      * 获取当前时间戳，单位秒
+     *
      * @return
      */
     public static long getCurrentTimestamp() {
-        return System.currentTimeMillis()/1000;
+        return System.currentTimeMillis() / 1000;
     }
 
     /**
      * 获取当前时间戳，单位毫秒
+     *
      * @return
      */
     public static long getCurrentTimestampMs() {
@@ -291,6 +292,7 @@ public class WXPayUtil {
 
     /**
      * 获取请求的流信息(这里是微信发的xml格式所有只能使用流来读)
+     *
      * @param is
      * @return
      * @throws IOException
@@ -306,6 +308,6 @@ public class WXPayUtil {
         is.close();
         byte[] lens = baos.toByteArray();
         //内容乱码处理
-        return new String(lens,"UTF-8");
+        return new String(lens, "UTF-8");
     }
 }
