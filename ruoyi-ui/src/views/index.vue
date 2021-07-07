5a44ec74a613311e05907524bba703b54d0bdd6a
ayay230 <398529803@qq.com>
2021-07-07 16:25:11 +0800

    修改前端首页内容

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -3,45 +3,25 @@
     <el-row :gutter="20">
       <el-col :sm="24" :lg="24">
         <blockquote class="text-warning" style="font-size: 14px">
-          领取阿里云通用云产品1888优惠券
-          <br />
           <el-link
-            href="https://www.aliyun.com/minisite/goods?userCode=brki8iof"
+            href="https://www.baidu.com"
             type="primary"
             target="_blank"
-            >https://www.aliyun.com/minisite/goods?userCode=brki8iof</el-link
+            >https://www.baidu.com</el-link
           >
           <br />
-          领取腾讯云通用云产品2860优惠券
-          <br />
-          <el-link
-            href="https://cloud.tencent.com/redirect.php?redirect=1025&cps_key=198c8df2ed259157187173bc7f4f32fd&from=console"
-            type="primary"
-            target="_blank"
-            >https://cloud.tencent.com/redirect.php?redirect=1025&cps_key=198c8df2ed259157187173bc7f4f32fd&from=console</el-link
-          >
-          <br />
-          阿里云服务器折扣区
-          <el-link href="http://aly.ruoyi.vip" type="primary" target="_blank"
-            >>☛☛点我进入☚☚</el-link
-          >
-          &nbsp;&nbsp;&nbsp; 腾讯云服务器秒杀区
-          <el-link href="http://txy.ruoyi.vip" type="primary" target="_blank"
-            >>☛☛点我进入☚☚</el-link
-          ><br />
           <h4 class="text-danger">
-            云产品通用红包，可叠加官网常规优惠使用。(仅限新用户)
+            el-link标签的使用方法
           </h4>
         </blockquote>
-
         <hr />
       </el-col>
     </el-row>
     <el-row :gutter="20">
       <el-col :sm="24" :lg="12" style="padding-left: 20px">
-        <h2>若依后台管理框架</h2>
+        <h2>桃子后台管理框架</h2>
         <p>
-          一直想做一款后台管理系统，看了很多优秀的开源项目但是发现没有合适自己的。于是利用空闲休息时间开始自己写一套后台系统。如此有了若依管理系统。，她可以用于所有的Web应用程序，如网站管理后台，网站会员中心，CMS，CRM，OA等等，当然，您也可以对她进行深度定制，以做出更强系统。所有前端后台代码封装过后十分精简易上手，出错概率低。同时支持移动客户端访问。系统会陆续更新一些实用功能。
+          基于若依后台管理系统进行魔改，新增大量常用内容，提升开发效率，纯分享，不作商业用途，有兴趣请支持若依框架作者。
         </p>
         <p>
           <b>当前版本:</b> <span>v{{ version }}</span>
@@ -55,15 +35,8 @@
             size="mini"
             icon="el-icon-cloudy"
             plain
-            @click="goTarget('https://gitee.com/y_project/RuoYi-Vue')"
+            @click="goTarget('https://gitee.com/taozi945/ruo-yi-vue')"
             >访问码云</el-button
-          >
-          <el-button
-            size="mini"
-            icon="el-icon-s-home"
-            plain
-            @click="goTarget('http://ruoyi.vip')"
-            >访问主页</el-button
           >
         </p>
       </el-col>
@@ -82,6 +55,7 @@
               <li>Spring Security</li>
               <li>JWT</li>
               <li>MyBatis</li>
+              <li>MyBatis-plus</li>
               <li>Druid</li>
               <li>Fastjson</li>
               <li>...</li>
@@ -112,30 +86,30 @@
           <div class="body">
             <p>
               <i class="el-icon-s-promotion"></i> 官网：<el-link
-                href="http://www.ruoyi.vip"
+                href="https://gitee.com/taozi945/ruo-yi-vue"
                 target="_blank"
-                >http://www.ruoyi.vip</el-link
+                >https://gitee.com/taozi945/ruo-yi-vue</el-link
               >
             </p>
             <p>
-              <i class="el-icon-user-solid"></i> QQ群：<s>满937441</s>
-              <s>满887144332</s> <s>满180251782</s> <s>满104180207</s>
-              <s>满186866453</s>
-              <a href="https://jq.qq.com/?_wv=1027&k=5vYAqA05" target="_blank">
-                201396349</a
+              <i class="el-icon-user-solid"></i> 
+              QQ群：
+              <!-- <s>满937441</s> -->
+              <a href="https://jq.qq.com/?_wv=1027&k=ru1P9q6n" target="_blank">
+                820080257</a
               >
             </p>
             <p>
               <i class="el-icon-chat-dot-round"></i> 微信：<a
                 href="javascript:;"
-                >/ *若依</a
+                >cytltyc</a
               >
             </p>
             <p>
               <i class="el-icon-money"></i> 支付宝：<a
                 href="javascript:;"
                 class="支付宝信息"
-                >/ *若依</a
+                >398529803@qq.com</a
               >
             </p>
           </div>
