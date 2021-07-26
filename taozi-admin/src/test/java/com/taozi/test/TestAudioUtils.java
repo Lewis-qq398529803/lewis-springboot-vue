a08191d4cf9f75a5c0087f224453611930d79abf
Ben527 <398529803@qq.com>
2021-07-26 16:54:20 +0800

    新增音频工具类

---
+++ taozi-admin/src/test/java/com/taozi/test/TestAudioUtils.java
@@ -0,0 +1,25 @@
+package com.taozi.test;
+
+import com.taozi.common.utils.file.AudioUtils;
+import org.junit.Test;
+
+
+/**
+ * MP3转PCM
+ *
+ * @author TAOZI
+ */
+public class TestAudioUtils {
+    //测试播放音频
+    @Test
+    public void testPaly() throws Exception {
+        AudioUtils utils = AudioUtils.getInstance();
+        utils.playMP3("D:/xx.mp3");
+    }
+
+    @Test
+    public void testConvert() throws Exception {
+        AudioUtils utils = AudioUtils.getInstance();
+        utils.convertMP32Pcm("D:/xx.mp3", "D:/xx.pcm");
+    }
+}
