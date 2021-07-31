fba1196848f982c571bfe4c548aa3bdc5ff30f09
Ben527 <398529803@qq.com>
2021-07-31 18:35:37 +0800

    日志列表支持排序操作

--- taozi-common/src/main/java/com/taozi/common/core/page/PageDomain.java
+++ taozi-common/src/main/java/com/taozi/common/core/page/PageDomain.java
@@ -8,62 +8,70 @@ import com.taozi.common.utils.StringUtils;
  * @author taozi
  */
 public class PageDomain {
-    /**
-     * 当前记录起始索引
-     */
-    private Integer pageNum;
+	/**
+	 * 当前记录起始索引
+	 */
+	private Integer pageNum;
 
-    /**
-     * 每页显示记录数
-     */
-    private Integer pageSize;
+	/**
+	 * 每页显示记录数
+	 */
+	private Integer pageSize;
 
-    /**
-     * 排序列
-     */
-    private String orderByColumn;
+	/**
+	 * 排序列
+	 */
+	private String orderByColumn;
 
-    /**
-     * 排序的方向desc或者asc
-     */
-    private String isAsc = "asc";
+	/**
+	 * 排序的方向desc或者asc
+	 */
+	private String isAsc = "asc";
 
-    public String getOrderBy() {
-        if (StringUtils.isEmpty(orderByColumn)) {
-            return "";
-        }
-        return StringUtils.toUnderScoreCase(orderByColumn) + " " + isAsc;
-    }
+	public String getOrderBy() {
+		if (StringUtils.isEmpty(orderByColumn)) {
+			return "";
+		}
+		return StringUtils.toUnderScoreCase(orderByColumn) + " " + isAsc;
+	}
 
-    public Integer getPageNum() {
-        return pageNum;
-    }
+	public Integer getPageNum() {
+		return pageNum;
+	}
 
-    public void setPageNum(Integer pageNum) {
-        this.pageNum = pageNum;
-    }
+	public void setPageNum(Integer pageNum) {
+		this.pageNum = pageNum;
+	}
 
-    public Integer getPageSize() {
-        return pageSize;
-    }
+	public Integer getPageSize() {
+		return pageSize;
+	}
 
-    public void setPageSize(Integer pageSize) {
-        this.pageSize = pageSize;
-    }
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+	}
 
-    public String getOrderByColumn() {
-        return orderByColumn;
-    }
+	public String getOrderByColumn() {
+		return orderByColumn;
+	}
 
-    public void setOrderByColumn(String orderByColumn) {
-        this.orderByColumn = orderByColumn;
-    }
+	public void setOrderByColumn(String orderByColumn) {
+		this.orderByColumn = orderByColumn;
+	}
 
-    public String getIsAsc() {
-        return isAsc;
-    }
+	public String getIsAsc() {
+		return isAsc;
+	}
 
-    public void setIsAsc(String isAsc) {
-        this.isAsc = isAsc;
-    }
+	public void setIsAsc(String isAsc) {
+		// 兼容前端排序类型
+		String isAsc1 = "ascending";
+		String isAsc2 = "descending";
+		if (isAsc1.equals(isAsc)) {
+			isAsc = "asc";
+		} else if (isAsc2.equals(isAsc)) {
+			isAsc = "desc";
+		}
+		this.isAsc = isAsc;
+	}
 }
