160a722dace07300286870a1398336196d005843
Ben527 <398529803@qq.com>
2021-08-02 10:56:16 +0800

    更多操作按钮添加权限控制

--- taozi-ui/src/views/system/user/index.vue
+++ taozi-ui/src/views/system/user/index.vue
@@ -256,10 +256,7 @@
                 v-hasPermi="['system:user:remove']"
                 >删除</el-button
               >
-              <el-dropdown
-                size="mini"
-                @command="(command) => handleCommand(command, scope.row)"
-              >
+              <el-dropdown size="mini" @command="(command) => handleCommand(command, scope.row)" v-hasPermi="['system:user:resetPwd', 'system:user:edit']">
                 <span class="el-dropdown-link">
                   <i class="el-icon-d-arrow-right el-icon--right"></i>更多
                 </span>
