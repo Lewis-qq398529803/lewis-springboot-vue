377a2dbf37c302466c370f6ece50cc1180ef98a2
Ben527 <398529803@qq.com>
2021-08-02 11:14:57 +0800

    密码框新增显示切换密码图标

--- taozi-ui/src/views/system/user/index.vue
+++ taozi-ui/src/views/system/user/index.vue
@@ -256,7 +256,11 @@
                 v-hasPermi="['system:user:remove']"
                 >删除</el-button
               >
-              <el-dropdown size="mini" @command="(command) => handleCommand(command, scope.row)" v-hasPermi="['system:user:resetPwd', 'system:user:edit']">
+              <el-dropdown
+                size="mini"
+                @command="(command) => handleCommand(command, scope.row)"
+                v-hasPermi="['system:user:resetPwd', 'system:user:edit']"
+              >
                 <span class="el-dropdown-link">
                   <i class="el-icon-d-arrow-right el-icon--right"></i>更多
                 </span>
@@ -358,6 +362,7 @@
                 placeholder="请输入用户密码"
                 type="password"
                 maxlength="20"
+                show-password
               />
             </el-form-item>
           </el-col>
@@ -457,10 +462,17 @@
         <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
         <div class="el-upload__tip text-center" slot="tip">
           <div class="el-upload__tip" slot="tip">
-            <el-checkbox v-model="upload.updateSupport" /> 是否更新已经存在的用户数据
+            <el-checkbox v-model="upload.updateSupport" />
+            是否更新已经存在的用户数据
           </div>
           <span>仅允许导入xls、xlsx格式文件。</span>
-          <el-link type="primary" :underline="false" style="font-size:12px;vertical-align: baseline;" @click="importTemplate">下载模板</el-link>
+          <el-link
+            type="primary"
+            :underline="false"
+            style="font-size: 12px; vertical-align: baseline"
+            @click="importTemplate"
+            >下载模板</el-link
+          >
         </div>
       </el-upload>
       <div slot="footer" class="dialog-footer">
